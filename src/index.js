b7391b4895303b626595194cda1a850a35c8c73e
Ian Jones <ianj1993@gmail.com>
2025-05-12 20:53:58 -0600

    Added header, and sidebar, as well as cleaned up file structure more

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import createUI from './modules/ui';
+
+createUI();
+
---
+++ src/modules/ui.js
@@ -0,0 +1,37 @@
+import '../styles.css';
+
+function createUI() {
+    createHeader();
+    createFooter();
+    createSidebar();
+}
+
+function createHeader() {
+    const headerDiv = document.createElement("div");
+    const header = document.createElement("h1");
+    header.textContent = "Todoloo";
+    headerDiv.appendChild(header);
+    headerDiv.classList.add("header");
+    document.body.appendChild(headerDiv);
+}
+
+function createFooter() {
+    const footerDiv = document.createElement("div");
+    const footer = document.createElement("p");
+    footer.innerHTML = '<a href="https://github.com/eonflip">Github</a>';
+    footerDiv.appendChild(footer);
+    footer.classList.add("footer");
+    document.body.appendChild(footerDiv);
+}
+
+function createSidebar() {
+    const sidebarDiv = document.createElement("div");
+    const sidebar = document.createElement("h2");
+    sidebar.textContent = "Projects";
+    sidebarDiv.appendChild(sidebar);
+    sidebarDiv.classList.add("sidebar");
+    document.body.appendChild(sidebarDiv);
+}
+
+
+export default createUI;
